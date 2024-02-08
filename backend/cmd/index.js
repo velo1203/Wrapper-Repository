bdf77005b88716843e037468559a72266d25ba4c
velo1203 <hokitoki1203@gmail.com>
2024-02-08 20:36:29 +0900

    some changed

--- backend/cmd/index.js
+++ backend/cmd/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config({ path: ".env" }); // .env 파일에서 환경변수 불러오기
 const authRouter = require("../internal/routes/authRouter");
 const repositoryRouter = require("../internal/routes/repoRouter");
 const userRepoRouter = require("../internal/routes/userRepoRouter");
@@ -6,7 +7,7 @@ const errorHandler = require("../internal/middleware/errorHandler");
 const logRequest = require("../internal/middleware/log_request");
 const path = require("path");
 const app = express();
-const port = 5000;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json()); // JSON 요청 본문 파싱을 위한 미들웨어
 app.use(logRequest); // 로그 미들웨어 등록
