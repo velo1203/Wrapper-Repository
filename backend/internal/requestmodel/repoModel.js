3f615ea9a0569d182afac138d00b713e500e083a
velo1203 <hokitoki1203@gmail.com>
2024-02-17 13:51:20 +0900

    레포 한글 생성 방지

--- backend/internal/requestmodel/repoModel.js
+++ backend/internal/requestmodel/repoModel.js
@@ -8,10 +8,12 @@ class postRepoModel {
         if (!this.name || !this.description) {
             throw Error("name, description and userId are required");
         }
-        // 폴더 이름으로 사용할 수 없는 문자를 제외하는 정규식
-        const invalidCharsRegex = /[<>:"\/\\|?*]/;
+        // 폴더 이름으로 사용할 수 없는 문자와 한글을 제외하는 정규식
+        const invalidCharsRegex = /[<>:"\/\\|?*\uAC00-\uD7A3]/;
         if (invalidCharsRegex.test(this.name)) {
-            throw Error("name contains invalid characters");
+            throw Error(
+                "name contains invalid characters or Korean characters"
+            );
         }
 
         return true;
