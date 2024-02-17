7d5bf98c1ef4b867004c3672e2a982fdd0157b88
velo1203 <hokitoki1203@gmail.com>
2024-02-17 14:30:16 +0900

    error fixed, 폴더가 모두 압축 해제되는 에러 수정됨

--- backend/internal/utils/zipUtils.js
+++ backend/internal/utils/zipUtils.js
@@ -4,18 +4,10 @@ const path = require("path");
 
 exports.extractZipContents = async function (zipPath, extractPath) {
     return new Promise((resolve, reject) => {
-        let topFolderName = null;
-
         fs.createReadStream(zipPath)
             .pipe(unzipper.Parse())
             .on("entry", async (entry) => {
-                if (topFolderName === null) {
-                    const parts = entry.path.split("/");
-                    topFolderName = parts[0];
-                }
-
-                const entryPath = entry.path.replace(new RegExp(`^${topFolderName}/`), "");
-                const fullPath = path.join(extractPath, entryPath);
+                const fullPath = path.join(extractPath, entry.path); // 최상위 폴더를 제거하지 않고 전체 경로 사용
 
                 if (entry.type === "Directory") {
                     await fs.promises.mkdir(fullPath, { recursive: true });
@@ -26,12 +18,14 @@ exports.extractZipContents = async function (zipPath, extractPath) {
                         await fs.promises.mkdir(directory, { recursive: true });
                     } catch (err) {
                         // 디렉토리 생성 중 오류 처리 (디렉토리가 이미 존재하는 경우 무시)
-                        if (err.code !== 'EEXIST') throw err;
+                        if (err.code !== "EEXIST") throw err;
                     }
-                    entry.pipe(fs.createWriteStream(fullPath)).on('error', reject);
+                    entry
+                        .pipe(fs.createWriteStream(fullPath))
+                        .on("error", reject);
                 }
             })
             .on("error", reject)
-            .on("finish", resolve);
+            .on("finish", resolve); // 압축 해제 완료 후 최상위 폴더 이름 반환
     });
 };
